0459229bda124c7a22c94ca812b61ce8d644ad23
drinfret-cshawi <drinfret@cshawi.ca>
2022-10-27 13:06:50 -0400

    correction petite erreur labo 4 interface IDataRepo méthode Select

--- CollegeDataLib/IDataRepo.cs
+++ CollegeDataLib/IDataRepo.cs
@@ -3,7 +3,7 @@ namespace CollegeDataLib;
 public interface IDataRepo<TData, TKey>
 {
     public List<TData> Select();
-    public TData? Select(int id);
+    public TData? Select(TKey key);
     public TKey? Insert(TData data);
     public bool Update(TData data);
     public bool Delete(TData data);
